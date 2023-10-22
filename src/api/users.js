b1ef3a5f0838b42e197660df8f41ba6e12275dfb
FlakoBB <josephrbb99@gmail.com>
2023-10-21 21:44:37 -0600

    feat: validate login

--- src/api/users.js
+++ src/api/users.js
@@ -18,3 +18,25 @@ export const registerUser = async (data) => {
     throw error
   }
 }
+
+export const loginUser = async (data) => {
+  try {
+    const response = await fetch('http://localhost:8000/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    if (!response.ok) {
+      const error = await response.json()
+      throw new Error(error.non_field_errors)
+    }
+
+    return response.json()
+  } catch (error) {
+    console.log(error.message)
+    throw error
+  }
+}
--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,23 +1,48 @@
 'use client'
+import { loginUser } from '@/api/users'
 import styles from '@/styles/registerForm.module.scss'
 import Link from 'next/link'
-import { useId } from 'react'
+import { useId, useState } from 'react'
 
 const LoginForm = () => {
   const usernameID = useId()
   const passwordID = useId()
 
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setLoginData({
+      ...loginData,
+      [name]: value
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      await loginUser(loginData)
+      console.log('sesion iniciada')
+      event.target.reset()
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.inputGroup}>
         <label htmlFor={usernameID}>Usuario:<span>*</span></label>
-        <input id={usernameID} type='text' required />
+        <input id={usernameID} type='text' name='username' onChange={handleChange} required />
       </div>
       <div className={styles.inputGroup}>
         <label htmlFor={passwordID}>Contraseña:<span>*</span></label>
-        <input id={passwordID} type='password' required />
+        <input id={passwordID} type='password' name='password' onChange={handleChange} required />
       </div>
-      <button>Acceder</button>
+      <button type='submit'>Acceder</button>
       <p>¿No tienes una cuenta? <Link href='/registrar'>Registrate</Link>.</p>
     </form>
   )
